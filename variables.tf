7f3d000b460daaa0c17d0d55ea591fba8bc8e65f
Raghavendra puri <124024434+ragavendrapuri@users.noreply.github.com>
2024-12-05 00:35:36 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
